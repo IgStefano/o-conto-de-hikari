19731ba0563481d3db307dd9f8fb0b64fa182b97
Igor Stefano <igstefano90@live.com>
2021-11-05 09:43:00 -0300

    mudança de fontes, add botão modo escuro

--- Assets/Scripts/dist/game.dev.js
+++ Assets/Scripts/dist/game.dev.js
@@ -1,24 +1,29 @@
+"use strict";
+
 window.onload = function () {
   // Atributos da página
-  const textElement = document.getElementById("text");
-  const choicesElement = document.getElementById("choices");
-  const howToPlayElement = document.getElementById("how-to-play");
-  const disclaimerElement = document.getElementById("disclaimer");
-  const backElement = document.getElementById("back");
-  const bodyElement = document.getElementById("body");
-  let savedId;
+  var textElement = document.getElementById("text");
+  var choicesElement = document.getElementById("choices");
+  var howToPlayElement = document.getElementById("how-to-play");
+  var disclaimerElement = document.getElementById("disclaimer");
+  var backElement = document.getElementById("back");
+  var bodyElement = document.getElementById("body");
+  var savedId; // Regendo o modo escuro
 
-  // Regendo o modo escuro
-  const darkModeElement = document.getElementById("dark-mode");
-  const darkModeMenuElement = document.getElementById("dark-mode-menu");
-  let darkModeToggle = false;
+  var darkModeElement = document.getElementById("dark-mode");
+  var darkModeMenuElement = document.getElementById("dark-mode-menu");
+  var darkModeToggle = false; // Adiciona um modo escuro ao jogo, clicando no kanji de Hikari
 
-  // Adiciona um modo escuro ao jogo, clicando no kanji de Hikari
-  darkModeElement.addEventListener("mousedown", () => darkMode());
-  darkModeMenuElement.addEventListener("mousedown", () => darkMode());
+  darkModeElement.addEventListener("mousedown", function () {
+    return darkMode();
+  });
+  darkModeMenuElement.addEventListener("mousedown", function () {
+    return darkMode();
+  });
 
   function darkMode() {
     darkModeToggle = !darkModeToggle;
+
     if (darkModeToggle === true) {
       disclaimerElement.classList.remove("btn-dark");
       howToPlayElement.classList.remove("btn-dark");
@@ -44,70 +49,67 @@ window.onload = function () {
       darkModeElement.classList.remove("bg-light");
       darkModeElement.classList.add("text-white");
     }
-  }
+  } // Atributos do personagem & Status
 
-  // Atributos do personagem & Status
-  let moralElement = document.getElementById("moral");
 
-  let luckElement = document.getElementById("luck");
-
-  let placeElement = document.getElementById("place");
-
-  let actElement = document.getElementById("current-act");
-
-  // Atributos da lógica do jogo
+  var moralElement = document.getElementById("moral");
+  var luckElement = document.getElementById("luck");
+  var placeElement = document.getElementById("place");
+  var actElement = document.getElementById("current-act"); // Atributos da lógica do jogo
   // let isDeadly;
-  let nextPageId;
 
-  //   Para inicar o jogo
+  var nextPageId; //   Para inicar o jogo
+
   function startGame() {
     showCurrentPage(1);
     moralElement.innerText = 0;
     luckElement.innerText = 1;
     placeElement.innerText = "?";
     actElement.innerText = "I";
-  }
+  } // Para que o jogo saiba em que página o jogador se encontra
 
-  // Para que o jogo saiba em que página o jogador se encontra
+
   function showCurrentPage(index) {
-    const showPage = pages.find((currentPage) => currentPage.id === index - 1);
+    var showPage = pages.find(function (currentPage) {
+      return currentPage.id === index - 1;
+    }); // Mostra o texto
 
-    // Mostra o texto
-    textElement.innerText = showPage.text;
+    textElement.innerText = showPage.text; // Atualiza os valores de status
 
-    // Atualiza os valores de status
     if (showPage.place) {
       placeElement.innerText = showPage.place;
     }
+
     if (showPage.act) {
       actElement.innerText = showPage.act;
     }
+
     if (showPage.luck) {
       if (showPage.luck > 0) {
         luckAdd(showPage.luck);
       } else if (showPage.luck < 0 && luckElement.innerText >= 0) {
         luckElement.innerText--;
       }
-    }
+    } // Remove as opções padrão
 
-    // Remove as opções padrão
+
     while (choicesElement.firstChild) {
       choicesElement.removeChild(choicesElement.firstChild);
-    }
+    } // Mostra as opções
 
-    // Mostra as opções
 
-    pages[index - 1].choices.forEach((currentChoice) => {
-      const label = document.createElement("label");
+    pages[index - 1].choices.forEach(function (currentChoice) {
+      var label = document.createElement("label");
       label.innerText = currentChoice.text;
       label.classList.add("btn-secondary");
       label.classList.add("btn");
       label.classList.add("shadow");
-      label.addEventListener("mousedown", () => selectChoice(currentChoice));
+      label.addEventListener("mousedown", function () {
+        return selectChoice(currentChoice);
+      });
       choicesElement.appendChild(label);
-    });
+    }); // Para mostrar o menu principal
 
-    // Para mostrar o menu principal
     if (showPage.id === 0) {
       document.getElementById("menu-header").classList.remove("d-none");
       document.getElementById("status-header").classList.add("d-none");
@@ -117,27 +119,26 @@ window.onload = function () {
       document.getElementById("menu-header").classList.add("d-none");
       document.getElementById("status-header").classList.remove("d-none");
       document.getElementById("status-header").classList.add("invisible");
-      document.getElementById("continue").classList.remove("d-none");
+      document.getElementById("continue").classList.remove("d-none"); // Acrescentando os botões da parte de baixo
 
-      // Acrescentando os botões da parte de baixo
       if (showPage.id > 3) {
         document.getElementById("footer-buttons").classList.remove("d-none");
         document.getElementById("back").classList.add("d-none");
         document.getElementById("status-header").classList.remove("invisible");
       }
+
       if (showPage.id > pages.length - 4) {
         document.getElementById("back").classList.remove("d-none");
       }
     }
 
-    let currentId = pages[index - 1].id;
+    var currentId = pages[index - 1].id;
 
     if (currentId < pages.length - 4) {
       savedId = currentId + 1;
     }
-  }
+  } // Regendo os botões no fim da tela
 
-  // Regendo os botões no fim da tela
 
   function footerButtons(button) {
     if (button === "disclaimer") {
@@ -147,20 +148,20 @@ window.onload = function () {
     } else if (button === "back") {
       nextPageId = savedId;
     }
+
     showCurrentPage(nextPageId);
   }
 
-  disclaimerElement.addEventListener("mousedown", () =>
-    footerButtons("disclaimer")
-  );
+  disclaimerElement.addEventListener("mousedown", function () {
+    return footerButtons("disclaimer");
+  });
+  howToPlayElement.addEventListener("mousedown", function () {
+    return footerButtons("howToPlay");
+  });
+  backElement.addEventListener("mousedown", function () {
+    return footerButtons("back");
+  }); // Checa se passou no teste de atributo (teste com requerimento 0 sempre passam)
 
-  howToPlayElement.addEventListener("mousedown", () =>
-    footerButtons("howToPlay")
-  );
-
-  backElement.addEventListener("mousedown", () => footerButtons("back"));
-
-  // Checa se passou no teste de atributo (teste com requerimento 0 sempre passam)
   function checkStats(moralRequirement) {
     if (moralRequirement > 0) {
       return moralElement.innerText >= moralRequirement;
@@ -169,39 +170,30 @@ window.onload = function () {
     } else {
       return true;
     }
-  }
+  } // Checa se é possível acrescentar pontos de Sorte
 
-  // Checa se é possível acrescentar pontos de Sorte
+
   function luckAdd() {
     if (luckElement.innerText < 3) {
       luckElement;
       luckElement.innerText++;
     }
-  }
+  } // Modifica o valor da moral
 
-  // Modifica o valor da moral
+
   function moralChange(value) {
-    if (
-      value > 0 &&
-      moralElement.innerText < 5 &&
-      moralElement.innerText >= 0
-    ) {
+    if (value > 0 && moralElement.innerText < 5 && moralElement.innerText >= 0) {
       moralElement.innerText++;
-    } else if (
-      value < 0 &&
-      moralElement.innerText > -5 &&
-      moralElement.innerText <= 0
-    ) {
+    } else if (value < 0 && moralElement.innerText > -5 && moralElement.innerText <= 0) {
       moralElement.innerText--;
     }
-  }
-
-  // // Checa se uma decisão errada causou fim de jogo
+  } // // Checa se uma decisão errada causou fim de jogo
   // function checkDead() {
   //   return luckElement.innerText === 0 && isDeadly === true;
   // }
-
   // Selecionando escolha
+
+
   function selectChoice(choice) {
     // Checa se testes são necessários e se passou nos testes
     if (choice.test) {
@@ -212,17 +204,19 @@ window.onload = function () {
       }
     } else {
       nextPageId = choice.nextPage;
+
       if (choice.moral) {
         moralChange(choice.moral);
       }
-    }
+    } // Reinício do jogo
 
-    // Reinício do jogo
+
     if (nextPageId <= 0) {
       return startGame();
     } else {
       return showCurrentPage(nextPageId + 1);
     }
   }
+
   startGame();
 };
